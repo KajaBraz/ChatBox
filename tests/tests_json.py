1df1520c4e959d6cd58d38ea590cb06d2cd75828
Kuba <jakubbr@gmail.com>
2020-04-18 19:46:12 +0200

    tests fixed

--- tests/tests_json.py
+++ tests/tests_json.py
@@ -1,4 +1,5 @@
 import unittest
+
 import my_json
 
 
@@ -19,11 +20,10 @@ class MyTestCase(unittest.TestCase):
 
     def test_dict_to_json(self):
         d = {"nested": {"a": "a", "b": 1}, "field": False}
-        expected_json = '{"field": false, "nested": {"a": "a", "b":1}}'
+        as_json = my_json.to_json(d)
+        d_result = my_json.from_json(as_json)
 
-        result = my_json.to_json(d)
-
-        self.assertEqual(expected_json, result)
+        self.assertEqual(d, d_result)
 
     def test_is_proper_json(self):
         j1 = '{"message_type":"user", "message_value":"i am white", "message_receiver":"sheep"}'
@@ -35,12 +35,6 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(expected_res1, result1)
         self.assertEqual(expected_res2, result2)
-
-    def test_print_to_json(self):
-        print(my_json.to_json({'one': 1, 'two': 2}))
-
-    def test_print_from_jason(self):
-        print(my_json.from_json('{"one": 1, "two": 2}'))
 
 
 if __name__ == '__main__':
