dc93cc6c9cdc1514ac20f55539b3fe70bb9bdf9b
Kuba <jakubbr@gmail.com>
2020-04-19 00:33:23 +0200

    files formatted; not used file removed

--- src/server.py
+++ src/server.py
@@ -1,14 +1,13 @@
 import socket
 import src.my_json as my_json
 
-
 if __name__ == '__main__':
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = socket.gethostname()
     port = 9999
     s.bind((host, port))
-    s.listen(5) # queue up to 5 requests
+    s.listen(5)  # queue up to 5 requests
 
     while True:
         client_socket, client_address = s.accept()
@@ -19,12 +18,12 @@ if __name__ == '__main__':
                 data_str = data.decode("utf-8")
                 data_json = my_json.to_json(data_str)
                 if my_json.is_proper_json(data_json):
-                    if data_json["message_type"]=="message":
-                        s.sendto(data_json,client_socket2)
+                    if data_json["message_type"] == "message":
+                        s.sendto(data_json, client_socket2)
 
         finally:
             client_socket.close()
 
     # def logged_in():
-        # for c in clients:
-            #
+    # for c in clients:
+    #
