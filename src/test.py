eee6f6c9f034e764a737f1e092b13e486e3617a1
Kaja Braz <kajabraz@gmail.com>
2020-04-18 19:35:37 +0200

    file deleted

--- src/test.py
+++
@@ -1,8 +0,0 @@
-def adding(a, b):
-    return a + b
-
-
-print('plik z dodawaniem')
-
-if __name__ == '__main__':
-    print('tu jest main name')
---
+++ src/try_test.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    try:
+        t = [1,2,3]
+        print('start')
+        print(t[4])
+        print(x)
+        print('koniec')
+    except:
+        print('jset exception')
+    finally:
+        print('tu finally')
+    print('fini')
+
