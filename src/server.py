be5f5cb702c0fab583a9a1c8dc368a0f7aa67b9d
Kaja Braz <kajabraz@gmail.com>
2020-04-19 20:46:21 +0200

    user, receiver, ald all_clients handled; json to be changed

--- src/server.py
+++ src/server.py
@@ -4,6 +4,8 @@ import my_json
 
 if __name__ == '__main__':
 
+    all_clients = []
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = socket.gethostname()
     port = 9999
@@ -19,12 +21,21 @@ if __name__ == '__main__':
                 data_str = data.decode("utf-8")
                 data_json = my_json.to_json(data_str)
                 if my_json.is_proper_json(data_json):
+                    if data_json["message_type"]=="user"
+                        login = data_json["message_value"]
+                        logged_in(login)
+                    if data_json["message_type"]=="get_all_logged":
+                        s.sendto(all_clients, login)
                     if data_json["message_type"]=="message":
-                        s.sendto(data_json,client_socket2)
+                        message = data_json["message_value"]
+                        s.sendto(message,data_json["message_receiver"])
+
 
         finally:
             client_socket.close()
 
-    # def logged_in():
+def logged_in(login, all_clients):
+    if login not in all_clients:
+        all_clients.add(login)
         # for c in clients:
             #
