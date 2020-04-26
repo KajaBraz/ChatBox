2ae6352c10fe999aa5b83b1e1fcee0d68b43e394
Kaja Braz <kajabraz@gmail.com>
2020-04-26 23:50:03 +0200

    comments removed

--- src/client.py
+++ src/client.py
@@ -67,33 +67,3 @@ if __name__ == "__main__":
     while True:
         message = input()
         client.send_message(message, interlocutor_name)
-
-    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # server_addresss = ('localhost', 10000)
-    # s.connect(server_address)
-    #
-    # try:
-    #     d = {"message_type": "user", "message_value": "froggy"}
-    #     print(d)
-    #     d_json = my_json.to_json(d)
-    #     time.sleep(5)
-    #     s.sendall(d_json)
-    #     print('all sent')
-    #
-    #     d = {"message_type": "message", "message_value": "alpacas"}
-    #     print(d)
-    #     d_json = my_json.to_json(d)
-    #     time.sleep(5)
-    #     s.sendall(d_json)
-    #     print('all sent')
-    #
-    #     d = {"message_type": "message", "message_value": "sheep"}
-    #     print(d)
-    #     d_json = my_json.to_json(d)
-    #     time.sleep(5)
-    #     s.sendall(d_json)
-    #     print('all sent')
-    #
-    # finally:
-    #     print('closing sender')
-    #     s.close()
