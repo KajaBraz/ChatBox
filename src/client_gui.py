2d849155783def5fb011ba9ad272bc494652841a
Kuba <jakubbr@gmail.com>
2020-05-03 03:52:10 +0200

    gui as class

--- src/client_gui.py
+++ src/client_gui.py
@@ -1,30 +1,29 @@
 import tkinter as tk
 
-# class Gui(tk.Frame):
-#     def __init__(self):
-#         self.window =
 
-v = 0
-label = None
+class Gui(tk.Frame):
+    def __init__(self):
+        self.window = tk.Tk()
+        super().__init__(self.window)
+        self.v = 0
+        self.window.bind("<Return>", self.enter_action)
+        self.label = tk.Label(text="my label", width=10, height=10, bg="red", fg="yellow")
+        self.label.pack()
+        self.button = tk.Button(text="button", bg="blue", fg="red", command=self.button_command)
+        self.button.pack()
 
+    def button_command(self):
+        self.v = self.v + 1
+        self.label['text'] = self.v
 
-def button_command():
-    global v
-    v = v + 1
-    label['text'] = v
+    def enter_action(self, event):
+        self.v = self.v + 2
+        self.label['text'] = self.v
 
-
-def enter_action(event):
-    global v
-    v = v + 2
-    label['text'] = v
+    def start(self):
+        self.window.mainloop()
 
 
 if __name__ == '__main__':
-    window = tk.Tk()
-    window.bind("<Return>", enter_action)
-    label = tk.Label(text="my label", width=10, height=10, bg="red", fg="yellow")
-    label.pack()
-    button = tk.Button(text="button", bg="blue", fg="red", command=button_command)
-    button.pack()
-    window.mainloop()
+    gui = Gui()
+    gui.start()
