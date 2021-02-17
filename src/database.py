786a1eb7fb1260dc28b672ac2ab0f544999fc11d
KajaBraz <kajabraz@gmail.com>
2021-02-17 03:53:44 +0100

    TODO solved, lines formatted

--- src/database.py
+++ src/database.py
@@ -5,13 +5,12 @@ from datetime import datetime
 def my_database(name, action):
     """action - parameter that may be one of the following two strings: 'CREATE', 'DROP', or 'INSERT'"""
 
-    # TODO connect to specific database and create the tables in it
-    with create_engine('postgresql://postgres:1861@localhost', isolation_level='AUTOCOMMIT').connect() as connection:
+    with create_engine('postgresql://postgres:000@localhost', isolation_level='AUTOCOMMIT').connect() as connection:
         if action in ['CREATE', 'DROP']:
             query = text(f'{action} DATABASE {name}')
             connection.execute(query)
 
-    with create_engine(f'postgresql://postgres:1861@localhost/{name}',
+    with create_engine(f'postgresql://postgres:000@localhost/{name}',
                        isolation_level='AUTOCOMMIT').connect() as connection:
         metadata = MetaData(bind=connection)
 
@@ -53,16 +52,18 @@ def my_database(name, action):
                  'registration_date': datetime.now()},
             )
             data_messages = (
-            {'sender_id': 1, 'recipient': 'user2', 'message': 'hello user1 here', 'date_time': datetime.now()},
-            {'sender_id': 4, 'recipient': 'user3', 'message': 'hello user4 here', 'date_time': datetime.now()})
+                {'sender_id': 1, 'recipient': 'user2', 'message': 'hello user1 here', 'date_time': datetime.now()},
+                {'sender_id': 4, 'recipient': 'user3', 'message': 'hello user4 here', 'date_time': datetime.now()})
 
             for line in data_users:
                 connection.execute(text(
-                    f'{action} INTO users(login, encoded_password, salt, registration_date) VALUES(:login, :encoded_password, :salt, :registration_date)'),
+                    f'{action} INTO users(login, encoded_password, salt, registration_date) VALUES(:login,'
+                    f':encoded_password, :salt, :registration_date)'),
                     **line)
             for line in data_messages:
                 connection.execute(text(
-                    f'{action} INTO messages(sender_id, recipient, message, date_time) VALUES(:sender_id, :recipient, :message, :date_time)'),
+                    f'{action} INTO messages(sender_id, recipient, message, date_time) VALUES(:sender_id, :recipient,'
+                    f':message, :date_time)'),
                     **line)
 
 
@@ -80,6 +81,6 @@ def fill_with_examples(db_name):
 
 if __name__ == '__main__':
     # drop_my_database('chatbox_test_8')
-    # create_my_database('chatbox_test_11')
-    # fill_with_examples('chatbox_test_11')
+    # create_my_database('chatbox_test_12')
+    # fill_with_examples('chatbox_test_12')
     pass
