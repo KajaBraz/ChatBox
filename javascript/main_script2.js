2e9f66209002695a2076dfc5ed38397fdc991c18
KajaBraz <kajabraz@gmail.com>
2021-04-08 00:49:12 +0200

    unique user id to avoid users with the same login

--- javascript/main_script2.js
+++ javascript/main_script2.js
@@ -1,10 +1,11 @@
-function format_sent_message(my_login, value) {
-    return `${my_login}: ${value}`;
+function format_sent_message(id_lenght, my_login, value) {
+    var name = my_login.slice(0, -id_lenght)
+    return `${name}: ${value}`;
 }
 
 
-function append_div_messages(my_name, message, message_box_element, class_name) {
-    var m = format_sent_message(my_name, message);
+function append_div_messages(my_name, message, message_box_element, class_name, id_lenght) {
+    var m = format_sent_message(id_lenght, my_name, message);
     var node = document.createTextNode(m);
     var div = document.createElement("div");
     div.className = class_name;
@@ -16,12 +17,13 @@ function append_div_messages(my_name, message, message_box_element, class_name) 
 }
 
 
-function handle_receive(message, message_box_element, class_name) {
+function handle_receive(message, message_box_element, class_name, id_lenght) {
+    console.log('receiving');
     var m = JSON.parse(message);
     if (m["message_type"] == "message") {
         var name = m["message_sender"];
         var val = m["message_value"];
-        append_div_messages(name, val, message_box_element, class_name);
+        append_div_messages(name, val, message_box_element, class_name, id_lenght);
         message_box_element.scrollTo(0, message_box_element.scrollHeight);
     };
 }
@@ -32,11 +34,12 @@ function send_button(message_type, message_element, my_name, chat_name, websocke
     console.log(message);
     send_websocket(message_type, message, my_name, chat_name, websocket);
     message_element.value = "";
-    console.log('done');
+    console.log('websocket sent');
 }
 
 
 function send_websocket(message_type, message, sender, chat_destination, websocket) {
+    console.log('sending websocket');
     var json_message = {
         message_type: message_type,
         message_value: message,
@@ -48,9 +51,22 @@ function send_websocket(message_type, message, sender, chat_destination, websock
 }
 
 
-function connect(user_name, chat_name) {
+function generate_unique_id(n) {
+    var s = "qwertyuioopasdfghjklzxcvbnmWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+    var id = '';
+    for (let i = 0; i < n; i++) {
+        id += s[Math.floor(Math.random() * s.length)];
+    };
+    console.log(id);
+    return id;
+}
+
+
+function connect(user_name, chat_name, id_lenght) {
     if (user_name != "" && chat_name != "") {
-        var url = `ws://${server_address}/${chat_name}/${user_name}`;
+        var id = generate_unique_id(id_lenght);
+        login = user_name + id;
+        var url = `ws://${server_address}/${chat_name}/${login}`;
         console.log("url", url);
         webSocket = new WebSocket(url);
         webSocket.onopen = () => {
@@ -60,7 +76,7 @@ function connect(user_name, chat_name) {
             console.log("received");
             console.log(event);
             console.log(event.data);
-            handle_receive(event.data, all_messages_element, "message");
+            handle_receive(event.data, all_messages_element, "message", id_lenght);
         };
     };
 }
@@ -103,6 +119,8 @@ var private_chats = document.getElementById("privateChats");
 
 var login = "";
 var chat = "";
+var websocket;
+var id_lenght = 20;
 var server_address = "localhost:11000";
 var active_public_chats = [];
 var active_private_chats = [];
@@ -111,7 +129,7 @@ var active_private_chats = [];
 choose_name_button.onclick = () => {
     login = my_name_element.value;
     console.log(login);
-    connect(login, chat);
+    connect(login, chat, id_lenght);
 };
 
 
@@ -119,7 +137,7 @@ choose_chat_button.onclick = () => {
     chat_name_header.innerHTML = chat_destination_element.value;
     chat = chat_destination_element.value;
     console.log(chat);
-    connect(login, chat);
+    connect(login, chat, id_lenght);
     add_chat(chat);
 };
 
@@ -128,7 +146,7 @@ button_element.onclick = () => {
     send_button(
         "message",
         message_element,
-        my_name_element.value,
+        login,
         chat_destination_element.value,
         webSocket
     );
