aefd51f9338c2e78655cc246743ee41a1e1fe533
KajaBraz <kajabraz@gmail.com>
2021-09-26 22:41:49 +0200

    first selenium tests - server needs to be started manually, asyncio removed

--- chatbox_tests/disabled_test_selenium.py
+++ chatbox_tests/disabled_test_selenium.py
@@ -1,11 +1,9 @@
-import asyncio
 import os
+import time
 
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-
-from src import chatbox_websocket_server, database, helper_functions
 
 
 class TestState:
@@ -13,36 +11,22 @@ class TestState:
 
 
 @pytest.fixture
-async def state():
-    config_path = '../chatbox_config.json'
-    data = helper_functions.read_config(config_path)
+def state():
     state = TestState()
-    state.server = chatbox_websocket_server.Server(data)
-    if 'database' in data:
-        state.server.chatbox_database = database.ChatBoxDatabase(data)
-        state.server.conn = state.server.chatbox_database.connect()
-    await state.server.start(data['address']['name'], data['address']['port'])
-    await asyncio.sleep(1)
     options = Options()
-    # options.add_argument('--headless')
+    options.add_argument('--headless')  # don't use when you want to actually open and see the browser window
     state.driver = webdriver.Firefox(options=options)
 
-    # to use when you want to actually open the browser window
-    # state.driver = webdriver.Firefox()
-
-    relative_path = os.path.join(os.pardir, 'javascript','index.html')
+    relative_path = os.path.join(os.pardir, 'javascript', 'index.html')
     absolute_path = os.path.abspath(relative_path)
 
     state.driver.get(f'file:///{absolute_path}')
 
     yield state
     state.driver.quit()
-    state.server.stop()
-    await state.server.wait_stop()
 
 
-@pytest.mark.asyncio
-async def test_first_connection(state):
+def test_first_connection(state):
     chat_room = 'NewChat'
     login = 'UserName'
     login_elem = state.driver.find_element_by_id('login')
@@ -54,14 +38,13 @@ async def test_first_connection(state):
     login_elem.send_keys(login)
     chat_elem.send_keys(chat_room)
     connect_button_elem.click()
-    await asyncio.sleep(1)
+    time.sleep(1)
     assert header_elem.text == chat_room
     assert chat_list_elem.find_element_by_id(chat_room).text == chat_room
     assert user_list_elem.find_element_by_class_name('chatUser').text == login
 
 
-@pytest.mark.asyncio
-async def test_sending_message(state):
+def test_sending_message(state):
     # CONNECTION
     chat_room = 'NewChat'
     login = 'UserName'
@@ -71,7 +54,7 @@ async def test_sending_message(state):
     login_elem.send_keys(login)
     chat_elem.send_keys(chat_room)
     connect_button_elem.click()
-    await asyncio.sleep(1)
+    time.sleep(1)
 
     # MESSAGE
     message = 'This is a message'
@@ -81,8 +64,5 @@ async def test_sending_message(state):
     message_box = state.driver.find_element_by_id('receivedMessages')
     new_message.send_keys(message)
     send_button.click()
-    await asyncio.sleep(1)
-    print('***')
-    print(message_box.find_element_by_class_name('message').text)
-    print('***')
-    assert message == message_box.find_element_by_class_name('message').text[-message_span:]
+    time.sleep(1)
+    assert message == message_box.find_elements_by_class_name('message')[-1].text[-message_span:]
