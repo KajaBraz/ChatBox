ecaf97a062449d461e6349e81a84d567a0c4b820
KajaBraz <kajabraz@gmail.com>
2024-05-15 21:53:33 +0200

    Use config file instead of parameters; Get ip address with curl

--- scripts/restart_chatbox.sh
+++ scripts/restart_chatbox.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-# The script takes the following positional parameters:
-#  - new IP address
-#  - new subfolder of archive_logs (year_month format, e.g., 2023_09)
-#  - database login
-#  - database password
-#  - database name
+source restart.config
 
 echo -e "\nRestart process started. Working in the following directory:"
 pwd .
-#cd git/ChatBox
 
 # Kill all python processes (should be http and chatbox servers)
 python_PIDs=$( pidof python )
@@ -24,21 +18,25 @@ git restore .
 echo -e "\nPull latest changes"
 git pull
 
+# Get current ip address
+ip_address=$( curl ifconfig.me/ip )
+echo -e "\nGetting IP address: $ip_address"
+
 # Update the server's address
 echo -e "\nUpdate server address:"
-sed -i "s/SERVER_ADDRESS = .*/SERVER_ADDRESS = $1/" server_http/static/config.js
+sed -i "s/SERVER_ADDRESS = .*/SERVER_ADDRESS = $ip_address/" server_http/static/config.js
 sed -n 1p server_http/static/config.js
 
 # Update config data
 echo -e "\nUpdate config data:"
-sed -i "s/\"db_login\": .*/\"db_login\": \"$3\",/" chatbox_config.json
-sed -i "s/\"db_password\": .*/\"db_password\": \"$4\",/" chatbox_config.json
-sed -i "s/\"db_name\": .*/\"db_name\": \"$5\"/" chatbox_config.json
+sed -i "s/\"db_login\": .*/\"db_login\": \"$database_login\",/" chatbox_config.json
+sed -i "s/\"db_password\": .*/\"db_password\": \"$database_password\",/" chatbox_config.json
+sed -i "s/\"db_name\": .*/\"db_name\": \"$database_name\"/" chatbox_config.json
 sed -i "s/\"name\": .*/\"name\": \"0.0.0.0\",/" chatbox_config.json
 
 # Update Flask data
 echo -e "\nUpdate Flask data:"
-sed -i "s/app.run()/app.run(host='0.0.0.0', port='13297')/" server_http/endpoints.py
+sed -i "s/app.run()/app.run(host='0.0.0.0', port='11000')/" server_http/endpoints.py
 
 # Move the logs file to the most recent folder in archive
 last_dir=$( ls -rt logs_archive | tail -n 1 )
@@ -46,7 +44,7 @@ echo -e "\nMove Chatbox server logs to logs_archive/$last_dir"
 mv logs.log logs_archive/$last_dir
 
 # Create new dir with name defined as parameter
-logs_dir="logs_archive/$2"
+logs_dir="logs_archive/$logs_subdirectory"
 mkdir "$logs_dir"
 
 # Start http server and save log file in the above folder
