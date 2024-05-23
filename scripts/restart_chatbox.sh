53648b0400d34b74de0fcbaacb2919cefb9431ab
KajaBraz <kajabraz@gmail.com>
2024-05-23 21:40:36 +0200

    Add quotation marks when writing to config.js; Update spacing in log files break lines; Fix undoing skipping worktree

--- scripts/restart_chatbox.sh
+++ scripts/restart_chatbox.sh
@@ -42,8 +42,8 @@ git pull
 echo -e "\n"
 
 # Undo ignoring config files (include previously excluded files in order to avoid potential confusion)
-git update-index --skip-worktree chatbox_config.json
-git update-index --skip-worktree scripts/restart_chatbox.config
+git update-index --no-skip-worktree chatbox_config.json
+git update-index --no-skip-worktree scripts/restart_chatbox.config
 
 # Get current ip address
 ip_address=$( curl ifconfig.me/ip )
@@ -51,7 +51,7 @@ echo -e "\nGet IP address: $ip_address"
 
 # Update the server's address
 echo -e "\nUpdate server address:"
-sed -i "s/SERVER_ADDRESS = .*/SERVER_ADDRESS = $ip_address:11000/" server_http/static/config.js
+sed -i "s/SERVER_ADDRESS = .*/SERVER_ADDRESS = \"$ip_address:11000\"/" server_http/static/config.js
 sed -n 1p server_http/static/config.js
 
 # Update config data
@@ -66,15 +66,15 @@ sed -i "s/app.run()/app.run(host='0.0.0.0', port='13297')/" server_http/endpoint
 latest_commit=$( git log -n1 --pretty=format:%h )
 logs_path_chatbox=$(  grep "log_file_name" chatbox_config.json | awk -F[\"\"] '{print $4}' )
 cur_date=$( date )
-echo -e "\n\n**** NEW VERSION ****\nRestart date: $cur_date\nLatest commit: $latest_commit\n\n" >> $logs_path_http
-echo -e "\n\n**** NEW VERSION ****\nRestart date: $cur_date\nLatest commit: $latest_commit\n\n" >> $logs_path_chatbox
+echo -e "\n\n**** NEW VERSION ****\n\tRestart date:\t$cur_date\n\tLatest commit:\t$latest_commit\n\n" >> $logs_path_http
+echo -e "\n\n**** NEW VERSION ****\n\tRestart date:\t$cur_date\n\tLatest commit:\t$latest_commit\n\n" >> $logs_path_chatbox
 
 # Start http server
-python -m server_http.endpoints >> $logs_path_http 2>&1 &
+python -m server_http.endpoints >> $logs_path_http 2>&1 &&
 echo -e "\nServer http started. Logs are being saved in $logs_path_http"
 
 # Start chatbox server
-python -m src.chatbox_websocket_server chatbox_config.json 2>&1 &
+python -m src.chatbox_websocket_server chatbox_config.json 2>&1 &&
 echo -e "\nChatbox server started. Logs are being saved in $logs_path_chatbox"
 
 echo -e "\nChatbox restart complete. Link: $ip_address:13297"
